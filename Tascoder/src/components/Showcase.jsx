c389b6e1edae0a428a3a2b9aab4492e6f9b5cc6a
m-inic <93374647+m-inic@users.noreply.github.com>
2026-03-24 22:27:53 +0100

    gotov showcase

--- Tascoder/src/components/Showcase.jsx
+++ Tascoder/src/components/Showcase.jsx
@@ -1,12 +1,76 @@
 import "./Showcase.css";
+import { useState } from "react";
 import desktop from "../assets/desktop.png";
-import android from "../assets/android.png"
-import appleIOS from "../assets/appleIOS.png"
-import huaweiMoj from "../assets/huaweiMoj.png"
-import browser from "../assets/browser.png"
-import mail from "../assets/mail.png"
+import android from "../assets/android.png";
+import appleIOS from "../assets/appleIOS.png";
+import huaweiMoj from "../assets/huaweiMoj.png";
+import browser from "../assets/browser.png";
+import mail from "../assets/mail.png";
+import deffender from "../assets/deffender.jpg";
+import ferrari from "../assets/ferrari.jpg";
+import landCruiser from "../assets/landCruiser.jpg";
+import nsx from "../assets/nsx.jpg";
+import r8Audi from "../assets/r8Audi.jpg";
+import sportage from "../assets/sportage.jpg";
 
 export default function Showcase() {
+  const [listItemActive, setListItemActive] = useState(1);
+
+  const showcaseSlides = [
+    {
+      id: 1,
+      src: desktop,
+      text: "Desktop",
+      slideSrc: ferrari,
+    },
+    {
+      id: 2,
+      src: android,
+      text: "Android",
+      slideSrc: deffender,
+    },
+    {
+      id: 3,
+      src: appleIOS,
+      text: "iOS",
+      slideSrc: landCruiser,
+    },
+    {
+      id: 4,
+      src: huaweiMoj,
+      text: "Wearables",
+      slideSrc: nsx,
+    },
+    {
+      id: 5,
+      src: browser,
+      text: "Browser extensions",
+      slideSrc: r8Audi,
+    },
+    {
+      id: 6,
+      src: mail,
+      text: "Email add-ons",
+      slideSrc: sportage,
+    },
+  ];
+
+  const showCaseSectionSlides = showcaseSlides.map((el) => (
+    <li
+      key={el.id}
+      className={
+        "showcaseSectionListItem" +
+        `${listItemActive === el.id ? "Active" : ""}`
+      }
+      onClick={() => setListItemActive(el.id)}
+    >
+      <img src={el.src} alt="" />
+      <p>{el.text}</p>
+    </li>
+  ));
+
+  const activeSlide = showcaseSlides.find((el) => el.id === listItemActive);
+
   return (
     <section id="showcaseSection">
       <div id="showcaseSectionTitleHolder">
@@ -17,35 +81,15 @@ export default function Showcase() {
         </p>
       </div>
       <div id="showcaseSectionNav">
-        <ul id="showcaseSectionList">
-          <li key="1">
-            <img src={desktop} alt="" />
-            <p>Desktop</p>
-          </li>
-          <li key="2">
-            <img src={android} alt="" />
-            <p>Android</p>
-          </li>
-          <li key="3">
-            <img src={appleIOS} alt="" />
-            <p>iOS</p>
-          </li>
-          <li key="4">
-            <img src={huaweiMoj} alt="" />
-            <p>Wearables</p>
-          </li>
-          <li key="5">
-            <img src={browser} alt="" />
-            <p>Browser extensions</p>
-          </li>
-          <li key="6">
-            <img src={mail} alt="" />
-            <p>Email add-ons</p>
-          </li>
-        </ul>
+        <ul id="showcaseSectionList">{showCaseSectionSlides}</ul>
       </div>
       <div id="showcaseSectionGallery">
-        <div></div>
+        <img
+        key={listItemActive}
+          src={activeSlide.slideSrc}
+          alt=""
+          className="animate__animated animate__fadeIn"
+        />
       </div>
     </section>
   );
