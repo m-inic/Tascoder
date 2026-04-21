700fb7b33a5290984146b65a21b6c481190f69e2
m-inic <93374647+m-inic@users.noreply.github.com>
2026-04-21 20:38:29 +0200

    support page gotov

--- Tascoder/src/pages/support/Support.jsx
+++ Tascoder/src/pages/support/Support.jsx
@@ -1,11 +1,20 @@
-import "./Support.css"
+import "./Support.css";
 import Footer from "../../components/Footer";
 import MainHeader from "../../components/MainHeader";
+import AvailableOptions from "../../components/AvailableOptions";
+import typewriter from "../../assets/typewriter-.png"
+
 export default function Support() {
-    return (
-        <>
-          <MainHeader />;
-          <Footer />
-        </>
-      );
+  return (
+    <>
+      <MainHeader />
+      <div id="availableOptionsTitleHolder">
+        <h2>SUPPORT</h2>
+        <img src={typewriter} alt="" />
+      </div>
+      <AvailableOptions></AvailableOptions>
+      <div></div>
+      <Footer />
+    </>
+  );
 }
