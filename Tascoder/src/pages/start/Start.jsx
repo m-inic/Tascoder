ccce41dd82e45c2645be68c12f8ea31c47abd68a
m-inic <93374647+m-inic@users.noreply.github.com>
2026-04-25 23:03:25 +0200

    radim start now

--- Tascoder/src/pages/start/Start.jsx
+++ Tascoder/src/pages/start/Start.jsx
@@ -1,12 +1,19 @@
-import "./Start.css"
+import "./Start.css";
 import Footer from "../../components/Footer";
 import MainHeader from "../../components/MainHeader";
+import logo from "../../assets/tascoderLogo.png";
 
 export default function Start() {
-    return (
-        <>
-          <MainHeader />;
-          <Footer />
-        </>
-      );
+  return (
+    <>
+      <MainHeader />
+      <section id="startNowSection">
+        <div id="startNowTitleHolder" >
+          <h2>tascoder</h2>
+          <img src={logo} alt="" />
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
 }
